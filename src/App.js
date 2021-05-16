a08811d53535059411395083b28394e9a2df4020
Ross Keenan <ross.keenan@protonmail.com>
2021-05-16 10:31:11 -0500

    Added dynamic output of list data using props.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import "./App.css";
 import GoalList from "./components/GoalList";
 
 const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&A" },
+  ];
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      {/* Goals is the prop and value reference is courseGoals */}
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
