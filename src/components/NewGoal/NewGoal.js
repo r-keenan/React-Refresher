06191f5f5199391c0994632dd20e72bf4311f258
Ross Keenan <ross.keenan@protonmail.com>
2021-05-16 11:28:09 -0500

    This adds two-way binding to the input field.

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NewGoal.css";
 
 const NewGoal = (props) => {
+  const [userInput, setUserInput] = useState("");
+
   const addGoalHandler = (e) => {
     //This prevents the submitting of the form to the server.
+
     e.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: "My new goal!",
+      text: userInput,
     };
+
+    setUserInput("");
+
     console.log(newGoal);
 
     props.onAddGoal(newGoal);
   };
+
+  const textChangedHandler = (e) => {
+    setUserInput(e.target.value);
+  };
+
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={userInput} onChange={textChangedHandler} />
       <button type="submit">Add Goal</button>
     </form>
   );
