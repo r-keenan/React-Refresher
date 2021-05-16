0350601e25584320b006975bb8aeb35b0b1e2881
Ross Keenan <ross.keenan@protonmail.com>
2021-05-16 09:29:05 -0500

    Broke out GoalLIst into its own component.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import GoalList from "./components/GoalList";
 
 const App = () => {
-  return <h1 title="This works!">Hi, this is ReactJS!</h1>;
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <GoalList />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/GoalList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish the Course</li>
+      <li>Learn all about the Course Main Topic</li>
+      <li>Help other students in the Course Q&A</li>
+    </ul>
+  );
+};
+
+export default GoalList;
