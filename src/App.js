65b692c5376ea7123d46daaa29b9ccac730f8441
Ross Keenan <ross.keenan@protonmail.com>
2021-05-16 11:00:13 -0500

    Data now passes from parent to child component. Console logs the updated array.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import GoalList from "./components/GoalList";
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
 
 const App = () => {
   const courseGoals = [
@@ -8,10 +9,17 @@ const App = () => {
     { id: "cg2", text: "Learn all about the Course Main Topic" },
     { id: "cg3", text: "Help other students in the Course Q&A" },
   ];
+  //This is a callback. Needs to be used when passing data from parent to child component.
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+    console.log(courseGoals);
+  };
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      {/* Goals is the prop and value reference is courseGoals */}
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      {/* Goals is the prop and value reference is courseGoals. Passes data from child to parent component */}
       <GoalList goals={courseGoals} />
     </div>
   );
---
+++ src/components/NewGoal/NewGoal.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./NewGoal.css";
+
+const NewGoal = (props) => {
+  const addGoalHandler = (e) => {
+    //This prevents the submitting of the form
+    e.preventDefault();
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: "My new goal!",
+    };
+    console.log(newGoal);
+
+    props.onAddGoal(newGoal);
+  };
+  return (
+    <form className="new-goal" onSubmit={addGoalHandler}>
+      <input type="text" />
+      <button type="submit">Add Goal</button>
+    </form>
+  );
+};
+
+export default NewGoal;
